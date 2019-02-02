4e39732953447696d2c0c9aa80f6a0e4380d5bff
bruinen <max.machnik@gmail.com>
2019-02-02 16:00:14 +0100

    First build

---
+++ build/precache-manifest.5ae3016f60ec634507695c233ab0c908.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba5cbb1df44c735b616dbc91458592ab",
+    "url": "/static/media/slide_0.ba5cbb1d.jpg"
+  },
+  {
+    "revision": "8219773c0e3698dcd5bc",
+    "url": "/static/css/main.e77b1aee.chunk.css"
+  },
+  {
+    "revision": "87b49ae0f00cc301368e",
+    "url": "/static/js/1.87b49ae0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3a285fb5971dfeedad3df064fa244fd",
+    "url": "/static/media/logo.d3a285fb.png"
+  },
+  {
+    "revision": "8219773c0e3698dcd5bc",
+    "url": "/static/js/main.8219773c.chunk.js"
+  },
+  {
+    "revision": "726af728b993a9011310660b5c6834da",
+    "url": "/static/media/slide_1.726af728.jpg"
+  },
+  {
+    "revision": "dfe23bebb526c6292cdffa2f81d72e17",
+    "url": "/static/media/slide_2.dfe23beb.jpg"
+  },
+  {
+    "revision": "8f0a4367e56b81d8db1b11425b72fb82",
+    "url": "/static/media/slide_3.8f0a4367.jpg"
+  },
+  {
+    "revision": "ff3d166d55abcab816c5eb5e9f7b63fa",
+    "url": "/static/media/slide_4.ff3d166d.jpg"
+  },
+  {
+    "revision": "0caf5eb5b4ebbbf7b52522846dcc32bc",
+    "url": "/index.html"
+  }
+];
